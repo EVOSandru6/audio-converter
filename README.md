adde7f637e283de0386393774a8836e1b2bcbad0
EVOSandru6 <evosduple@mail.ru>
2021-06-16 00:26:23 +0300

    refactor

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 ### Convert audio road in video files
 
+```
+sudo apt-get install -y ffmpeg
+```
+
+В папке с фильмами:
+```
+mkdir convert
+```
+
+Вызов по относительному пути:
+```
+/path/to/start.py
+```
--- start.py
+++ start.py
@@ -1,30 +1,28 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import os
 import subprocess
 
-dir_path='.'
+dir_path = '.'
 
 for root, dirs, files in os.walk(dir_path):
     for file in files:
-
-        if file == 'Thumbs.db':	
+        if file == 'Thumbs.db':
             continue
         if file == 'convert':
             continue
 
-        tmp=file.split('.')
-        tt=tmp[:(len(tmp)-1)]
-        res_name='.'.join(tt);
+        tmp = file.split('.')
+        tt = tmp[:(len(tmp) - 1)]
+        res_name = '.'.join(tt)
 
         file = file.replace("(", "\(")
         file = file.replace(")", "\)")
         file = file.replace(" ", "\ ")
 
-        #file = file.encode('string-escape')
+        # file = file.encode('string-escape')
 
-        print 'Start.............' + file
-        bc = 'ffmpeg -i ' + str(file) + ' -vcodec copy -acodec libmp3lame -ar 44100 -ac 2 -ab 192k convert/'+ str(file)
-        print 'Process.............' + bc
+        print('Start.............' + file)
+        bc = 'ffmpeg -i ' + str(file) + ' -vcodec copy -acodec libmp3lame -ar 44100 -ac 2 -ab 192k convert/' + str(file)
+        print('Process.............' + bc)
 
-        subprocess.call(bc,shell=True)
-
+        subprocess.call(bc, shell=True)
